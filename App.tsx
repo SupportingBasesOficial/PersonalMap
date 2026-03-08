c0ce580f312606ac2895469f274c72bc73de89e5
SupportingBasesOficial <suportingbases@gmail.com>
2026-03-08 02:10:52 +0000

    Fix app entry to render MapScreen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import MapScreen from "./src/screens/MapScreen"
+import React from "react";
+import MapScreen from "./src/screens/MapScreen";
 
 export default function App() {
-  return <MapScreen />
+      return <MapScreen />;
 }
